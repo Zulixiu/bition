a0eec75fbc3c836323bc26d726732a3dc16f4acb
sq <1498917969@qq.com>
2019-06-15 09:07:51 +0800

    first sql

---
+++ src/main/java/io/sq/bitcoinexplorer0613/Bitcoinexplorer0613Application.java
@@ -0,0 +1,13 @@
+package io.sq.bitcoinexplorer0613;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bitcoinexplorer0613Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bitcoinexplorer0613Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/sq/bitcoinexplorer0613/Bitcoinexplorer0613ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.sq.bitcoinexplorer0613;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bitcoinexplorer0613ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
