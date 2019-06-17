74c048645122e19ab25d8329cabffcbc15fa8527
sq <1498917969@qq.com>
2019-06-17 08:53:00 +0800

    first api2

---
+++ src/main/java/io/sq/bitcoinexplorer0613/controller/TempController.java
@@ -0,0 +1,24 @@
+package io.sq.bitcoinexplorer0613.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import io.sq.bitcoinexplorer0613.api.BitcoinRestApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/temp")
+public class TempController {
+
+    @Autowired
+    private BitcoinRestApi bitcoinRestApi;
+
+    @GetMapping("/test")
+    public String test(){
+//        JSONObject blockChainInfo = bitcoinRestApi.getBlockChainInfo();
+        JSONObject blockNoTxDetails = bitcoinRestApi.getBlockNoTxDetails("00000000000ed3139372c0b8449f0fb1778b128f247546a551316edf5e531fb7");
+
+        return blockNoTxDetails.toJSONString();
+    }
+}
